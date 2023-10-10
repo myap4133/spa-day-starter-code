21b7f87fd7d98628907de705a5918ced06d19763
may2fn <may2fn@umsystem.edu>
2023-10-09 20:38:24 -0500

    Finished What I could for Spa Day User Validation Studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,20 +3,28 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+import java.sql.SQLOutput;
 import java.util.Objects;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @PostMapping("")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        if(user.getPassword().equals(verify)){
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if(errors.hasErrors()){
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "user/add";
+        }
+        if(user.getPassword().equals(user.getVerify())){
             model.addAttribute("username", user.getUsername());
             return "user/index";
         }
@@ -27,7 +35,8 @@ public class UserController {
     }
 
     @GetMapping("add")
-    public String displayAddUserForm(){
+    public String displayAddUserForm(Model model){
+        model.addAttribute(new User());
         return "user/add";
     }
 
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,14 +1,41 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
 public class User {
+
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 to 15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters.")
     private String password;
 
-    public User(String username, String email, String password) {
+    @NotBlank(message = "Verify Password is required.")
+    private String verify;
+
+    public User(){
+        id = nextId;
+        nextId++;
+    }
+
+    public User(String username, String email, String password, String verify) {
+        this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -31,4 +58,11 @@ public class User {
         return password;
     }
 
+    public String getVerify() {
+        return verify;
+    }
+
+    public Boolean checkPasswords(){
+        return Objects.equals(password, verify);
+    }
 }
